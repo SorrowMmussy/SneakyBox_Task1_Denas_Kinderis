4c45c2929675cc2ab3af0b3c2e50f3334e73bacb
Denas Kinderis <denas.kinderis@gmail.com>
2018-11-14 15:16:23 +0200

    MainWindow_Loaded to start filling only when WPF is loaded
    Rotation/Skewing
    Populating cells with int/string.

--- SneakyBox_Task1_Denas_Kinderis/MainWindow.xaml.cs
+++ SneakyBox_Task1_Denas_Kinderis/MainWindow.xaml.cs
@@ -24,28 +24,53 @@ namespace SneakyBox_Task1_Denas_Kinderis
         {
             InitializeComponent();
         }
+        //-------------------------------------------------------------------------
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            DG_Main.Items.Clear();
+            for (int i = 0; i < int.Parse(TB_Rows.Text); i++)
+            {
+                DG_Main.Items.Add(new MyData("SB"));
+            }
+            DG_Main.Columns.Clear();
+            for (int i = 0; i < int.Parse(TB_Columns.Text); i++)
+            {
+                var column = new DataGridTextColumn();
+                column.Binding = new Binding("MyNumber");
+                column.Width = new DataGridLength(1, DataGridLengthUnitType.Star);
+                DG_Main.Columns.Add(column);
+            }
+        }
+        //-------------------------------------------------------------------------
         //Columns
         private void TB_Columns_TextChanged(object sender, TextChangedEventArgs e)
         {
-            int columns;
-
+            if (DG_Main == null)
+            {
+                return;
+            }
+            int columns = 0;
             if (!int.TryParse(TB_Columns.Text, out columns))
             {
                 return;
             }
-            
             DG_Main.Columns.Clear();
-
             for (int i = 0; i < columns; i++)
             {
                 var column = new DataGridTextColumn();
+                column.Binding = new Binding("MyNumber");
+                column.Width = new DataGridLength(1, DataGridLengthUnitType.Star);
                 DG_Main.Columns.Add(column);
             }
         }
         //Rows
         private void TB_Rows_TextChanged(object sender, TextChangedEventArgs e)
         {
-            int rows;
+            if (DG_Main == null)
+            {
+                return;
+            }
+            int rows = 0;
             if (!int.TryParse(TB_Rows.Text, out rows))
             {
                 return;
@@ -53,18 +78,29 @@ namespace SneakyBox_Task1_Denas_Kinderis
             DG_Main.Items.Clear();
             for (int i = 0; i < rows; i++)
             {
-                DG_Main.Items.Add((i));
+                DG_Main.Items.Add(new MyData("SB"));
             }
         }
-
+        //Rotate
         private void S_Rotation_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-
+            if (DG_Main == null)
+            {
+                return;
+            }
+            double angle = S_Rotation.Value;
+            //RotateTransform rtt = new RotateTransform(angle);
+            SkewTransform skewTransform1 = new SkewTransform(angle, 0, -0.5, 0.5);
+            DG_Main.RenderTransform = skewTransform1;
         }
-
-        private void DG_Main_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        //-------------------------------------------------------------------------
+        class MyData
         {
-
+            public string MyNumber { get; set; }
+            public MyData(string n)
+            {
+                MyNumber = n;
+            }
         }
     }
 }
